fc002c6f1120223094127a60c65318489320090c
Bogdan Voinalovych <bogdan.voinalovych@gmail.com>
2021-02-05 19:58:21 +0200

    init_commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className="jumbotron">
+      <div className="container">
+        <h1 className="display-4">Информация</h1>
+        <p className="lead">Here`s supposed to be some information. So let`s call it 'whatever information' and consider this page proper :)</p>
+      </div>
+    </div>
+  )
+}
